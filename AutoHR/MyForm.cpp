4eff36d97285baca0f344999b5a9b01cf9cbe336
Addeline <lilykhalilova3121@gmail.com>
2026-04-26 17:50:08 +0500

    Fixed MainForm deletion and restored navigation logic

--- AutoHR/MyForm.cpp
+++ AutoHR/MyForm.cpp
@@ -5,16 +5,15 @@ using namespace System;
 using namespace System::Windows::Forms;
 
 [STAThread]
-int main(array<String^>^ args) {
-    Application::EnableVisualStyles();
-    Application::SetCompatibleTextRenderingDefault(false);
-    AutoHR::MyForm form;
-    Application::Run(% form);
-    return 0;
+void main(array<String^>^ args) {
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	AutoHR::MyForm form;
+	Application::Run(% form);
 }
 
 // Код перехода
-System::Void AutoHR::MyForm::linkLabel2_LinkClicked(System::Object^ sender, System::Windows::Forms::LinkLabelLinkClickedEventArgs^ e) {
+void AutoHR::MyForm::linkLabel2_LinkClicked(System::Object^ sender, System::Windows::Forms::LinkLabelLinkClickedEventArgs^ e) {
     AutoHR::MyForm1^ regForm = gcnew AutoHR::MyForm1();
-    regForm->ShowDialog();
+	regForm->ShowDialog();
 }
